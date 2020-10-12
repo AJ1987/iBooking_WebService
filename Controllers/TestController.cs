e06848ac367cdf4b071645b06732ad38343efcbd
AJ1987 <6680718@qq.com>
2020-10-13 02:34:11 +0800

    feat: init iBooking Web Service

---
+++ Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iBooking_WebService.Controllers
+{
+    /// <summary>
+    ///  Controller for test
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        /// <summary>
+        /// Get test
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            return "AJ";
+        }
+
+        /// <summary>
+        /// Post test
+        /// </summary>
+        [HttpPost]
+        public void Post()
+        { }
+
+        /// <summary>
+        ///  Put test
+        /// </summary>
+        [HttpPut]
+        public void Put() { }
+
+        /// <summary>
+        ///  Delete test
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public bool Del()
+        {
+            return true;
+        }
+    }
+}
